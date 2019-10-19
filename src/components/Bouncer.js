e44a6d8e8608c38e44f17193ddce647c1d39ddfa
olacodes <olatundesodiq@gmail.com>
2019-10-19 16:57:13 +0100

    Add bouncer component

---
+++ src/components/Bouncer.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import "./Bouncer.css"
+function Bouncer(){
+    return (
+        <div className="bouncer-main-container">
+            <div className="bouncer-container">
+                <h2 className="bouncer">Bouncer</h2>
+                <div className="nav-container">
+                    <li className="home">Home</li>
+                    <li>store</li>
+                    <li>iphone</li>
+                    <li>Ipad</li>
+                    <li>macbook</li>
+                    <li>Accessories</li>
+                </div>  
+            </div>
+        </div>
+        
+    )
+}
+export default Bouncer
