e80709fb74bc6936f5e4e32664951f8bd32fb2ac
olacodes <olatundesodiq@gmail.com>
2019-10-21 05:16:04 +0100

    modify bouncer component

--- src/components/Bouncer.js
+++ src/components/Bouncer.js
@@ -1,21 +1,19 @@
 import React from 'react';
+import { navLinks } from "./bouncer.json"
 import "./Bouncer.css"
-function Bouncer(){
+function Bouncer() {
     return (
         <div className="bouncer-main-container">
             <div className="bouncer-container">
                 <h2 className="bouncer">Bouncer</h2>
                 <div className="nav-container">
-                    <li className="home">Home</li>
-                    <li>store</li>
-                    <li>iphone</li>
-                    <li>Ipad</li>
-                    <li>macbook</li>
-                    <li>Accessories</li>
-                </div>  
+                    {navLinks.map((navLink, index) => (
+                        <li key={index}>{navLink}</li>
+                    ))}
+                </div>
             </div>
         </div>
-        
+
     )
 }
 export default Bouncer
