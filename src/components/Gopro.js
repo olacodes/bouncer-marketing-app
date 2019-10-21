fc23458a1a57011257013588d34716a4258871df
olacodes <olatundesodiq@gmail.com>
2019-10-21 05:14:08 +0100

    add GoPro component

--- src/components/Gopro.js
+++ src/components/Gopro.js
@@ -1,11 +1,34 @@
 import React from "react"
+import goPros from "./goPros.js"
 import "./Gopro.css"
+
+
 
 function Gopro(){
     return(
-        <div className="container">
-            <div className="iphone-6">
-                <div className="iphone__text__container">
+            <div className="goPro-container">
+                {goPros.map((goPro)=> (
+                <div key={goPro.id}
+                id={goPro.id}
+                className="goPro">
+                    <div 
+                    
+                    className="goPro-header-para">
+
+                        <h4 className="goPro__header">{goPro.name}</h4>
+                        <p className="goPro__para">{goPro.description}</p>
+                        <p className="goPro__price">{goPro.price}</p>
+                    </div>
+                   <div 
+                        className="goPro__image" style={{ backgroundImage: `url(${goPro.image})`
+                        }}>
+                    </div>
+
+                    
+                </div>
+
+                ))}
+                {/* <div className="iphone__text__container">
                     <h4 className="iphone6__header">iPhone 6</h4>
                     <p className="iphone__para">
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, labor
@@ -13,11 +36,19 @@ function Gopro(){
                     <p className="iphone__price">$399</p>
                 </div>
                 <div className="iphone6-image-container">
-                    {/* <img className="iphone__6__img" src={require("../assets/image/iPhone6s_discover_desktop_08_iOS9-1.png")} alt="iphone-6"/> */}
-                </div>
-                <div style={{ backgroundImage: `url(require("../assets/image/iPhone6s_discover_desktop_08_iOS9-1.png"))` }}>ihdosfcjsdpo</div>
+                    <div 
+                        className="iphone__6__img" style={{ backgroundImage: `url(${require(`${"../assets/image/iphone6.png"}`)})`, 
+                        backgroundPosition: 'top',
+                        backgroundSize: "cover",
+                        height: '20rem',
+                        width: "26rem"}}>
+                    </div>
+                    
+                </div> */}
+               
             </div>
-        </div>
+
+            
     )
 }
 
