f73b8b014227523d83320cb60cabb3a6ec68c932
olacodes <olatundesodiq@gmail.com>
2019-10-26 23:43:41 +0100

    add typography  component

---
+++ src/components/Typography/Typography.js
@@ -0,0 +1,65 @@
+import styled from "styled-components"
+
+let H1 = styled.h1`
+  font-size: 56px;
+  font-weight: 600;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.32;
+  letter-spacing: 1.4px;
+  text-align: center;
+  color: #22262a;
+`
+let H2 = styled.h2`
+  font-size: 48px;
+  font-weight: 600;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.33;
+  letter-spacing: 1.2px;
+  text-align: center;
+  color: #22262a;
+`
+
+let H3 = styled.h3`
+    font-size: 36px;
+    font-weight: 600;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.33;
+  letter-spacing: 1.2px;
+  text-align: center;
+  color: #22262a;
+`
+let H4 = styled.h4`
+    font-size: 30px;
+    font-weight: 400;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.33;
+  letter-spacing: 1.2px;
+  text-align: center;
+  color: #22262a;
+`
+let H5 = styled.h5`
+    font-size: 24px;
+    font-weight: 600;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.33;
+  letter-spacing: 1.2px;
+  text-align: center;
+  color: #22262a;
+`
+let H6 = styled.h6`
+    font-size: 18px;
+    font-weight: 600;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: 1.33;
+  letter-spacing: 1.2px;
+  text-align: center;
+  color: #22262a;
+`
+
+export {H1, H2, H3, H4, H5, H6}
